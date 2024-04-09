e90c5489cf8269716b1d9456771d9e818750e1d4
Radoslav Ivanov <radoslav4096@gmail.com>
2024-04-09 15:55:41 +0300

    [Singleton] Add implementations

--- Singleton/src/implementation/SingletonSimple.java
+++ Singleton/src/implementation/SingletonSimple.java
@@ -1,2 +1,17 @@
-package implementation;public class SingletonSimple {
+package implementation;
+
+public class SingletonSimple {
+  private static SingletonSimple instance;
+  private String data;
+
+  private SingletonSimple() {
+    this.data = "data";
+  }
+
+  public static SingletonSimple getInstance() {
+    if (instance == null) {
+      instance = new SingletonSimple();
+    }
+    return instance;
+  }
 }
--- Singleton/src/implementation/SingletonThreadSafe.java
+++ Singleton/src/implementation/SingletonThreadSafe.java
@@ -1,2 +1,25 @@
-package implementation;public class SingletonThreadSafe {
+package implementation;
+
+public class SingletonThreadSafe {
+  // volatile - the variable must be read from the memory every time (cannot be cached)
+  private static volatile SingletonThreadSafe instance;
+  private String data;
+
+  private SingletonThreadSafe() {
+    this.data = "data";
+  }
+
+  public static SingletonThreadSafe getInstance() {
+    // Introducing a local variable that can be cached to prevent reading multiple times from the memory when there is no need
+    SingletonThreadSafe result = instance;
+    if (result == null) {
+      synchronized (SingletonThreadSafe.class) {
+        result = instance;
+        if (result == null) {
+          instance = result = new SingletonThreadSafe();
+        }
+      }
+    }
+    return result;
+  }
 }
